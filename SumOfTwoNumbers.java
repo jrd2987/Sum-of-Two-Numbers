d4f011493073f23c0dc1117106732decbbacb0be
Justin Doggett <doggett.justin1987@gmail.com>
2017-05-17 21:20:13 -0500

    Sum of Two Numbers

---
+++ SumOfTwoNumbers.java
@@ -0,0 +1,21 @@
+/*
+Programmer: Justin Doggett
+Date: 5/3/2017
+Description: The program stores the integers 62 and 99 in variables, and stores their sum in a variable named total.
+*/
+
+public class SumOfTwoNumbers
+{
+	//Start of program execution
+	public static void main(String [] args)
+	{
+		int num1 = 62; // sets the value of 62 into the num1 variable
+		int num2 = 99; //sets the value of 99 into the num2 variable
+		int total = num1 + num2; //sets the sum of the num1 and num2 value to total
+
+		//displays the sum of 62 + 99 to the console
+		System.out.println("The sum of 62 + 99 is: " + total);
+	}
+}
+
+
